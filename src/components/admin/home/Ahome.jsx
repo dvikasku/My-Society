7689f7705d98763068f92487736763f7c789bac5
dvikasku <dvikasku@gmail.com>
2022-06-24 16:59:19 +0530

    widget

--- src/components/admin/home/Ahome.jsx
+++ src/components/admin/home/Ahome.jsx
@@ -1,5 +1,7 @@
 import Sidebar from '../../sidebar/Sidebar'
 import Navbar from '../../navbar/Navbar'
+import Widget from '../../widget/Widget'
+import Table from '../../notices/Table'
 import "./home.scss"
 
 const Ahome = () => {
@@ -8,7 +10,18 @@ const Ahome = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        Container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="table">
+          Notices
+          <Table />
+          {/* Complain
+          <Table /> */}
+        </div>
       </div>
     </div>
   )
